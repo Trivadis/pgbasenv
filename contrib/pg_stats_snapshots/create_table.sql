638a2d8e04c922de64c45b33cc3ab638290f9908
Roland Stirnimann <roland.stirnimann@trivadis.com>
2021-07-02 15:24:22 +0000

    Comment charachter replaced in sql files

--- contrib/pg_stats_snapshots/create_table.sql
+++ contrib/pg_stats_snapshots/create_table.sql
@@ -1,23 +1,23 @@
-# Copyright 2021 Trivadis AG <info@trivadis.com>
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Author:  Michael Muehlbeyer
-# Desc: readme.md for details
-#
-# Change log:
-#   29.06.2021: Michael: Initial version created
-#
+-- Copyright 2021 Trivadis AG <info@trivadis.com>
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     https://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Author:  Michael Muehlbeyer
+-- Desc: readme.md for details
+--
+-- Change log:
+--   29.06.2021: Michael: Initial version created
+--
 
 create table public.stat_statements_snapshots as
 select null::timestamp with time zone as created, *
