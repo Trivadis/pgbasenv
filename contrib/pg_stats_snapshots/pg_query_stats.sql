ff7e20bbf81cf908bf3982a44dbefa857c4e7be9
mmuehlbeyer <m.muehlbeyer@gmx.de>
2021-07-15 09:27:08 +0200

    added contrib (#5)
    * added contrib
    
    * Readme renamed from .txt to .md and Markdown syntax fixed.
    
    * License header added to script files
    
    * Make the shell scripts pgBasEnv compatible
    
    * Comment charachter replaced in sql files
    
    * Added some examples.
    
    Co-authored-by: mmuehlbeyer <mmuehlbeyer@users.noreply.github.com>
    Co-authored-by: Roland Stirnimann <roland.stirnimann@trivadis.com>

---
+++ contrib/pg_stats_snapshots/pg_query_stats.sql
@@ -0,0 +1,30 @@
+-- Copyright 2021 Trivadis AG <info@trivadis.com>
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     https://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Author:  Michael Muehlbeyer
+-- Desc: readme.md for details
+--
+-- Change log:
+--   29.06.2021: Michael: Initial version created
+--
+
+-- creates a function to query historical pg_stat_statements data 
+-- usage: SELECT public.query_stats('2021-05-11 13:00:00','2021-05-11 16:00:00')
+create or replace function public.query_stats(day1 character varying, day2 character varying)
+returns setof stat_statements_snapshots
+language sql
+as $function$
+select * from stat_statements_snapshots sss where sss.created between day1::timestamp and day2::timestamp order by 1 desc;
+$function$
+;
