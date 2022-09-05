a39c986cdbf6dd662de68f7c0d41553644320fad
cyrmue <71831183+cyrmue@users.noreply.github.com>
2022-09-05 17:38:43 +0200

    do not use lsof or netstat/ss if not needed (#12)
    * do not use lsof or netstat/ss if not needed
    
    The resource consumption of lsof/netstat/ss could be quite significant a lots of clusters and pids it could be one of the top commands, if you are using pgoperate and the daemon (which checks the state of the postgres).
    
    * Corrected variable name from ppid to pid
    
    Co-authored-by: Roland Stirnimann <roland.stirnimann@accenture.com>

--- bin/pgup.sh
+++ bin/pgup.sh
@@ -189,21 +189,32 @@ done
 }
 
 find_datadir_of_running_proc() {
-local d
-for d in $($LSOF -p $1 2> /dev/null | grep DIR | awk '{print $9}'); do
-  [[ -f $d/global/pg_control ]] && echo $d
-done
+  local pid
+  pid=$1
+  if [[ -h /proc/${pid}/cwd && -f /proc/${pid}/cwd/global/pg_control ]]; then
+    readlink -f /proc/${pid}/cwd
+  else
+    local d
+    for d in $($LSOF -p $1 2> /dev/null | grep DIR | awk '{print $9}'); do
+      [[ -f $d/global/pg_control ]] && echo $d
+    done
+  fi
 }
 
 
 find_port_of_running_proc() {
    local pid=$1
-   which netstat > /dev/null 2>&1
-   local RC=$?
-   if [[ $RC -eq 0 ]]; then
-     netstat -ltnp 2>/dev/null| grep -E "^tcp .* ${pid}" | awk '{print $4}' | cut -d":" -f2
+   if [[ -h /proc/${pid}/cwd && -f /proc/${pid}/cwd/postmaster.pid ]]; then
+     #using the port from pid
+     head -4 /proc/${pid}/cwd/postmaster.pid |tail -1
    else
-     ss -ltnp | grep "pid=${pid}," | awk '{print $4}' | cut -d":" -f2
+     which netstat > /dev/null 2>&1
+     local RC=$?
+     if [[ $RC -eq 0 ]]; then
+       netstat -ltnp 2>/dev/null| grep -E "^tcp .* ${pid}" | awk '{print $4}' | cut -d":" -f2
+     else
+       ss -ltnp | grep "pid=${pid}," | awk '{print $4}' | cut -d":" -f2
+     fi
    fi
 }
 
